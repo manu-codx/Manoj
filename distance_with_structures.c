fffde33bc6d22984315210291e3b3979b025d65d
manu-codx <75928156+manu-codx@users.noreply.github.com>
2021-02-10 11:19:05 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,39 @@
 //WAP to find the distance between two points using structures and 4 functions.
+
+
+
+#include<stdio.h>
+int height()
+{
+int h;
+printf("Enter h value");
+scanf("%d",&h);
+return h;
+}
+int base()
+{
+int b;
+printf("enter b value");
+scanf("%d",&b);
+return b;
+}
+int input()
+{
+int d;
+printf("enter d value");
+scanf("%d",&d);
+return d;
+}
+int main()
+{
+int x,y,z;
+double r,vol=0;
+x=height();
+y=base();
+z=input();
+vol=0.33*((x*y*z)+(z/y));
+
+printf("Volume is %lf",vol);
+
+return 0;
+}
