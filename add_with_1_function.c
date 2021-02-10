9db09124fe13e3fb9af00a5bd7cdd271e45f1f4c
manu-codx <75928156+manu-codx@users.noreply.github.com>
2021-02-10 11:22:35 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -1,1 +1,17 @@
-//Write a program to add two user input numbers using one function.
+//Write a program to add two user input numbers using one function
+
+
+include <stdio.h>
+ 
+void main()
+{
+    int i, num, sum = 0;
+ 
+    printf("Enter an integer number \n");
+    scanf ("%d", &num);
+    for (i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    printf ("Sum of first %d  numbers = %d\n", num, sum);
+}
